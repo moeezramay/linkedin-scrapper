438e9407c3f9b1455d5453306da2205f0ebdd761
TalhaMajeed55 <103556161+moeezramay@users.noreply.github.com>
2025-09-17 14:20:40 +0500

    Project forked from joeyism

---
+++ linkedin_scraper-master/test/scrape_person.py
@@ -0,0 +1,35 @@
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+import csv
+driver = webdriver.Chrome()
+
+email = "moeez@codexon.pk"
+password = "police15@SA"
+actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+person = Person("https://www.linkedin.com/in/mattsnowden/", driver=driver)
+
+print("Name:", person.name)
+print("About:", person.about)
+print("Experiences:", person.experiences)
+print("Educations:", person.educations)
+print("Interests:", person.interests)
+print("Accomplishments:", person.accomplishments)
+print("Jobs Title:", person.job_title)
+
+
+with open('person.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['Name', 'About', 'Experiences', 'Educations', 'Interests', 'Accomplishments', 'Job Title']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({
+        'Name': person.name,
+        'About': person.about,
+        'Experiences': person.experiences,
+        'Educations': person.educations,
+        'Interests': person.interests,
+        'Accomplishments': person.accomplishments,
+        'Job Title': person.job_title
+    })
+
+print("Data saved to person.csv")
