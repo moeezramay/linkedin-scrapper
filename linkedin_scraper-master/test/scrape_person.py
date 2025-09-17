681dff6b5738186069f9fa5bb54e6a01327cf7b2
TalhaMajeed55 <103556161+moeezramay@users.noreply.github.com>
2025-09-17 21:26:24 +0500

    security fixed

--- linkedin_scraper-master/test/scrape_person.py
+++ linkedin_scraper-master/test/scrape_person.py
@@ -1,27 +1,36 @@
-from linkedin_scraper import Person, actions
+from linkedin_scraper import Person, actions, Company
 from selenium import webdriver
 import csv
+import os
 driver = webdriver.Chrome()
 
-email = "moeez@codexon.pk"
-password = "police15@SA"
-actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-person = Person("https://www.linkedin.com/in/mattsnowden/", driver=driver)
+email = os.getenv("LINKEDIN_USER")
+password = os.getenv("LINKEDIN_PASSWORD")
+actions.login(driver, email, password) 
 
 
-with open('person.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Name', 'About', 'Experiences', 'Educations', 'Interests', 'Accomplishments', 'Job Title']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+company = Company("https://ca.linkedin.com/company/google")
 
-    writer.writeheader()
-    writer.writerow({
-        'Name': person.name,
-        'About': person.about,
-        'Experiences': person.experiences,
-        'Educations': person.educations,
-        'Interests': person.interests,
-        'Accomplishments': person.accomplishments,
-        'Job Title': person.job_title
-    })
+print(company.employees)
 
-print("Data saved to person.csv")
+
+
+#person = Person("https://www.linkedin.com/in/mattsnowden/", driver=driver)
+
+
+#with open('person.csv', 'w', newline='', encoding='utf-8') as csvfile:
+#    fieldnames = ['Name', 'About', 'Experiences', 'Educations', 'Interests', 'Accomplishments', 'Job Title']
+#    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#
+#    writer.writeheader()
+#    writer.writerow({
+#        'Name': person.name,
+#        'About': person.about,
+#        'Experiences': person.experiences,
+#        'Educations': person.educations,
+#       'Interests': person.interests,
+#        'Accomplishments': person.accomplishments,
+#        'Job Title': person.job_title
+#    })
+
+#print("Data saved to person.csv")
