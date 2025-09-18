0dbdf681416838173962a0d33bddfa5b5e46aca4
TalhaMajeed55 <103556161+moeezramay@users.noreply.github.com>
2025-09-18 16:02:59 +0500

    untraceable chromedriver added

--- linkedin_scraper-master/test/scrape_person.py
+++ linkedin_scraper-master/test/scrape_person.py
@@ -1,8 +1,33 @@
+import undetected_chromedriver as uc
 from linkedin_scraper import Person, actions, Company
-from selenium import webdriver
 import csv
 import os
-driver = webdriver.Chrome()
+import time
+
+options = uc.ChromeOptions()
+
+driver = uc.Chrome(options=options)
+
+# Add options to make the browser appear more like a regular user
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+# You might want to add a user-agent to appear more like a regular browser
+options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+# Initialize the undetected driver
+driver = uc.Chrome(options=options)
+
+# Execute CDP commands to further reduce detection
+driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+    'source': '''
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        })
+    '''
+})
+
 
 email = os.getenv("LINKEDIN_USER")
 password = os.getenv("LINKEDIN_PASSWORD")
